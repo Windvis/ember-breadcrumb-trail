c7bcc765bc69d4dfdddfb4ea6bc27562dceb5d06
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-05-15 10:34:42 +0200

    Don't run qunit through babel
    https://github.com/embroider-build/embroider/issues/731#issuecomment-808788950

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -15,5 +15,11 @@ module.exports = function (defaults) {
   */
 
   const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app);
+  return maybeEmbroider(app, {
+    skipBabel: [
+      {
+        package: 'qunit', // More information: https://github.com/embroider-build/embroider/issues/731
+      },
+    ],
+  });
 };
