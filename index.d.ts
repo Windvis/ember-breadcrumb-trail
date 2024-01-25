0b799cf4b76ae914f67f093ce7c96e73a9967af6
Charles Fries <charliefries@icloud.com>
2024-01-24 18:55:47 -0800

    Add TypeScript types

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+declare module "ember-breadcrumb-trail/helpers/breadcrumb" {
+  import Helper from "@ember/component/helper";
+
+  export default class Breadcrumb extends Helper<{
+    Args: {
+      Positional: [title: string];
+      Named: Record<string, unknown>;
+    };
+    Return: void;
+  }> {}
+}
+
+declare module "ember-breadcrumb-trail/helpers/breadcrumbs" {
+  import Helper from "@ember/component/helper";
+
+  export default class Breadcrumbs extends Helper<{
+    Return: { title: string; data: { route: string; model: string } }[];
+  }> {}
+}
