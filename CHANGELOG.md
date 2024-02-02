5c87a96664412af2a1bcb901b64de2b07f18b4b0
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-02-02 21:30:59 +0100

    V2 addon conversion (#29)
    V2 conversion based on the latest v2 addon TS blueprint.

--- CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-# Changelog
-
-## v0.2.0 (2021-05-17)
-#### :boom: Breaking Change
-* [#12](https://github.com/Windvis/ember-breadcrumb-trail/pull/12) Remove the breadcrumb title spacing functionality ([@Windvis](https://github.com/Windvis))
-
-## v0.1.0 (2021-05-15)
-#### :rocket: Enhancement
-* [#1](https://github.com/Windvis/ember-breadcrumb-trail/pull/1) Initial implementation ([@Windvis](https://github.com/Windvis))
---
+++ addon/src/helpers/breadcrumb.js
@@ -0,0 +1,35 @@
+import Helper from '@ember/component/helper';
+import { inject as service } from '@ember/service';
+
+export default class BreadcrumbHelper extends Helper {
+  @service('breadcrumbs') breadcrumbsService;
+
+  breadcrumbId = null;
+
+  get breadcrumbWasAddedBefore() {
+    return Boolean(this.breadcrumbId);
+  }
+
+  compute(breadcrumbTitleParts, optionalData) {
+    let breadcrumbTitle = breadcrumbTitleParts.join('');
+
+    let breadcrumbData = {
+      data: optionalData,
+      title: breadcrumbTitle,
+    };
+
+    if (this.breadcrumbWasAddedBefore) {
+      this.breadcrumbsService.updateBreadcrumb(
+        this.breadcrumbId,
+        breadcrumbData,
+      );
+    } else {
+      this.breadcrumbId = this.breadcrumbsService.addBreadcrumb(breadcrumbData);
+    }
+  }
+
+  willDestroy() {
+    super.willDestroy(...arguments);
+    this.breadcrumbsService.removeBreadcrumb(this.breadcrumbId);
+  }
+}
