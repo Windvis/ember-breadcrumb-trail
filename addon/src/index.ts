276b80556d896022166b236be6f841f556b8f7c0
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-02-02 22:44:03 +0100

    TS conversion
    Co-authored-by: Charles Fries <charliefries@icloud.com>

---
+++ addon/src/index.ts
@@ -0,0 +1,3 @@
+// For some reason these imports make the build fail
+// export { default as breadcrumb } from './helpers/breadcrumb';
+// export { default as breadcrumbs } from './helpers/breadcrumbs';
--- addon/src/template-registry.ts
+++ addon/src/template-registry.ts
@@ -2,10 +2,11 @@
 // Add all your components, helpers and modifiers to the template registry here, so apps don't have to do this.
 // See https://typed-ember.gitbook.io/glint/environments/ember/authoring-addons
 
-// import type MyComponent from './components/my-component';
+// import type { breadcrumb, breadcrumbs } from './index';
+import type breadcrumb from './helpers/breadcrumb';
+import type breadcrumbs from './helpers/breadcrumbs';
 
-// Remove this once entries have been added! 👇
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export default interface Registry {
-  // MyComponent: typeof MyComponent
+  breadcrumb: typeof breadcrumb;
+  breadcrumbs: typeof breadcrumbs;
 }
