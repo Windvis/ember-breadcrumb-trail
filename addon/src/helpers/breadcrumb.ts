b75ebd12c9d95d6613fe0ec907a6375798ec5c7b
Charles Fries <charliefries@icloud.com>
2025-07-08 15:06:09 -0700

    Rm inject

--- addon/src/helpers/breadcrumb.ts
+++ addon/src/helpers/breadcrumb.ts
@@ -1,5 +1,5 @@
 import Helper from '@ember/component/helper';
-import { inject as service } from '@ember/service';
+import { service } from '@ember/service';
 import type { BreadcrumbData } from '../private-types.ts';
 import type BreadcrumbsService from '../services/breadcrumbs.ts';
 
--- test-app/app/routes/foo/bar/baz.js
+++ test-app/app/routes/foo/bar/baz.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
+import { service } from '@ember/service';
 
 export default class FooBarBazRoute extends Route {
   @service('breadcrumbs') breadcrumbsService;
